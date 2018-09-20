4a17c318c313a114e7e055b71dd237cf33e5c9e0
jekku <jekripreclaroorlina@gmail.com>
2018-09-20 14:45:47 +0800

    Uses supervisor. Blocks excess jobs.

--- lib/prodcon.ex
+++ lib/prodcon.ex
@@ -1,32 +1,33 @@
 defmodule Prodcon do
-  @moduledoc """
-  Documentation for Prodcon.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Prodcon.hello
-      :world
-
-  """
   def start(producer_count \\ 5, consumer_count \\ 5, queue_size \\ 10) do
-    GenServer.start_link(
-      Queue,
-      %{
-        producer_count: consumer_count,
-        consumer_count: consumer_count,
-        queue_size: queue_size
-      },
-      name: :jequeue
-    )
+    {:ok, queue} = GenServer.start_link(Queue, %{queue_size: queue_size})
+    producers = Enum.map(1..producer_count, &create_producer(&1, queue))
+    consumers = Enum.map(1..consumer_count, &create_consumer(&1, queue))
+
+    Supervisor.start_link(producers ++ consumers, strategy: :one_for_one)
+  end
+
+  defp create_consumer(id, queue) do
+    %{
+      id: :"consumer_#{id}",
+      start: {Consumer, :start_link, [%{id: id, queue: queue}]}
+    }
+  end
+
+  defp create_producer(id, queue) do
+    %{
+      id: :"producer_#{id}",
+      start: {Producer, :start_link, [%{id: id, queue: queue}]}
+    }
   end
 end
 
 defmodule Producer do
   use GenServer
+
+  def start_link(defaults) do
+    GenServer.start_link(__MODULE__, defaults)
+  end
 
   @impl true
   def init(%{id: id, queue: queue} = state) do
@@ -50,13 +51,19 @@ end
 defmodule Consumer do
   use GenServer
 
+  def start_link(defaults) do
+    GenServer.start_link(__MODULE__, defaults)
+  end
+
   def init(%{id: id, queue: queue} = state) do
+    GenServer.cast(queue, {:consumer, id})
     {:ok, state}
   end
 
-  def handle_cast({:job, job, producer_id}, %{id: id, queue: queue}) do
+  def handle_cast({:job, job, producer_id}, %{id: id, queue: queue} = state) do
     IO.puts("\t\t\t\t\t\t\t\t\t\tCONSUMER #{id}: Received job #{job} from #{producer_id}")
-    Process.sleep(job)
+    Process.send_after(queue, {:consumer, id}, job)
+    {:noreply, state}
   end
 end
 
@@ -65,22 +72,32 @@ defmodule Queue do
 
   def init(
         %{
-          producer_count: producer_count,
-          consumer_count: consumer_count,
           queue_size: queue_size
         } = state
       ) do
-    for id <- 1..producer_count,
-        do: GenServer.start_link(Producer, %{id: "producer_#{id}", queue: self()})
+    {:ok, %{jobs: [], consumers: [], queue_size: queue_size}}
+  end
 
-    for id <- 1..consumer_count,
-        do: GenServer.start_link(Consumer, %{id: "consumer_#{id}", queue: self()})
+  def handle_cast({:consumer, consumer_id}, %{consumers: consumers, jobs: []} = state) do
+    {:noreply, %{state | consumers: [consumer_id | consumers]}}
+  end
 
-    {:ok, %{jobs: [], consumers: []}}
+  def handle_cast(
+        {:consumer, consumer_id},
+        %{consumers: consumers, jobs: [next_job | remaining_jobs]} = state
+      ) do
+    GenServer.cast(consumer_id, {:job, next_job})
+    {:noreply, %{state | consumers: [consumer_id | consumers], jobs: remaining_jobs}}
+  end
+
+  def handle_cast({:producer, job, producer_id}, %{jobs: jobs, queue_size: queue_size} = state)
+      when queue_size == length(jobs) do
+    IO.puts("Discarding job #{job} from #{producer_id}")
+    {:noreply, state}
   end
 
   def handle_cast({:producer, job, producer_id}, %{jobs: jobs} = state) do
-    IO.puts("Queueing job #{job} from PRODUCER #{producer_id}")
+    IO.puts("Queueing job #{job} from #{producer_id}")
     {:noreply, %{state | jobs: [job | jobs]}}
   end
 end
