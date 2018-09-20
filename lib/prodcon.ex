5a2a9130ccba4241f769f6503550a07c4f6c7d03
jekku <jekripreclaroorlina@gmail.com>
2018-09-20 17:03:18 +0800

    Use proper looping mechanism

--- lib/prodcon.ex
+++ lib/prodcon.ex
@@ -4,21 +4,20 @@ defmodule Prodcon do
     producers = Enum.map(1..producer_count, &create_producer(&1, queue))
     consumers = Enum.map(1..consumer_count, &create_consumer(&1, queue))
 
-    # Supervisor.start_link([queue] ++ producers ++ consumers, strategy: :one_for_one)
-    Supervisor.start_link([queue] ++ producers, strategy: :one_for_one)
+    Supervisor.start_link([queue] ++ producers ++ consumers, strategy: :one_for_one)
   end
 
   defp create_consumer(id, queue) do
     %{
       id: :"consumer_#{id}",
-      start: {Consumer, :start_link, [%{id: "consumer_#{id}", queue: Queue}]}
+      start: {Consumer, :start_link, [%{id: :"consumer_#{id}", queue: Queue}]}
     }
   end
 
   defp create_producer(id, queue) do
     %{
       id: :"producer_#{id}",
-      start: {Producer, :start_link, [%{id: "producer_#{id}", queue: Queue}]}
+      start: {Producer, :start_link, [%{id: :"producer_#{id}", queue: Queue}]}
     }
   end
 end
@@ -26,44 +25,65 @@ end
 defmodule Producer do
   use GenServer
 
-  def start_link(defaults) do
-    GenServer.start_link(__MODULE__, defaults)
+  def start_link(%{id: producer_id} = args) do
+    GenServer.start_link(__MODULE__, args, name: producer_id)
   end
 
   @impl true
   def init(%{id: id, queue: queue} = state) do
-    Task.async(fn -> schedule_work(id, queue) end)
+    schedule_work(id, queue)
     {:ok, state}
   end
 
   defp schedule_work(id, queue) do
     :rand.seed(:exs1024s)
-    job = :rand.uniform(10) * 100
-    IO.puts("\t\t\t\t\t#{id}: Generating job #{job}")
-    Process.sleep(job)
+    duration = :rand.uniform(10) * 100
+    IO.puts("\t\t\t\t\t#{id}: Generating job #{duration}")
+    Process.sleep(duration)
 
-    IO.puts("\t\t\t\t\t#{id}: Sending job #{job} to #{queue}")
-    GenServer.cast(queue, {:producer, job, id})
+    IO.puts("\t\t\t\t\t#{id}: Sending job #{duration} to #{queue}")
+    GenServer.cast(queue, {:producer, %{duration: duration, producer_id: id}})
 
+    Process.send_after(self(), :work, 1)
+  end
+
+  def handle_info(:work, %{id: id, queue: queue} = state) do
     schedule_work(id, queue)
+    {:noreply, state}
   end
 end
 
 defmodule Consumer do
   use GenServer
 
-  def start_link(defaults) do
-    GenServer.start_link(__MODULE__, defaults)
+  def start_link(%{id: consumer_id} = args) do
+    GenServer.start_link(__MODULE__, args, name: consumer_id)
   end
 
   def init(%{id: id, queue: queue} = state) do
-    GenServer.cast(queue, {:consumer, id})
+    IO.puts("\t\t\t\t\t\t\t\t\t\t#{id}: Ready")
+    schedule_consumption(id, queue)
     {:ok, state}
   end
 
-  def handle_cast({:job, job, producer_id}, %{id: id, queue: queue} = state) do
-    IO.puts("\t\t\t\t\t\t\t\t\t\tCONSUMER #{id}: Received job #{job} from #{producer_id}")
-    Process.send_after(queue, {:consumer, id}, job)
+  defp schedule_consumption(id, queue) do
+    IO.puts("\t\t\t\t\t\t\t\t\t\t#{id}: Ready")
+    GenServer.cast(queue, {:consumer, id})
+
+    Process.send_after(self(), :work, 1)
+  end
+
+  def handle_cast(
+        {:job, %{duration: duration, producer_id: producer_id}},
+        %{id: id, queue: queue} = state
+      ) do
+    IO.puts("\t\t\t\t\t\t\t\t\t\t#{id}: Received job #{duration} from #{producer_id}")
+    Process.sleep(duration)
+    {:noreply, state}
+  end
+
+  def handle_info(:work, %{id: id, queue: queue} = state) do
+    schedule_consumption(id, queue)
     {:noreply, state}
   end
 end
@@ -84,25 +104,57 @@ defmodule Queue do
   end
 
   def handle_cast({:consumer, consumer_id}, %{consumers: consumers, jobs: []} = state) do
-    {:noreply, %{state | consumers: [consumer_id | consumers]}}
+    consume_job(%{state | consumers: [consumer_id | consumers]})
   end
 
   def handle_cast(
         {:consumer, consumer_id},
-        %{consumers: consumers, jobs: [next_job | remaining_jobs]} = state
+        %{consumers: consumers} = state
       ) do
-    GenServer.cast(consumer_id, {:job, next_job})
-    {:noreply, %{state | consumers: [consumer_id | consumers], jobs: remaining_jobs}}
+    consume_job(%{state | consumers: [consumer_id | consumers]})
   end
 
-  def handle_cast({:producer, job, producer_id}, %{jobs: jobs, queue_size: queue_size} = state)
-      when queue_size == length(jobs) do
-    IO.puts("Discarding job #{job} from #{producer_id}")
+  defp consume_job(%{jobs: []} = state) do
     {:noreply, state}
   end
 
-  def handle_cast({:producer, job, producer_id}, %{jobs: jobs} = state) do
-    IO.puts("Queueing job #{job} from #{producer_id}")
+  defp consume_job(
+         %{
+           jobs: [%{duration: duration, producer_id: producer_id} = next_job | remaining_jobs],
+           consumers: [next_consumer | remaining_consumers]
+         } = state
+       ) do
+    IO.puts("#{stats(state)} Sending job #{duration} from #{producer_id} to #{next_consumer}")
+    GenServer.cast(next_consumer, {:job, next_job})
+    {:noreply, %{jobs: remaining_jobs, consumers: remaining_consumers}}
+  end
+
+  def handle_cast(
+        {:producer, %{duration: duration, producer_id: producer_id}},
+        %{jobs: jobs, queue_size: queue_size} = state
+      )
+      when queue_size == length(jobs) do
+    IO.puts("#{stats(state)} Discarding job #{duration} from #{producer_id}")
+    {:noreply, state}
+  end
+
+  def handle_cast(
+        {:producer, %{duration: duration, producer_id: producer_id} = job},
+        %{jobs: []} = state
+      ) do
+    IO.puts("#{stats(state)} Queueing job #{duration} from #{producer_id}")
+    {:noreply, %{state | jobs: [job]}}
+  end
+
+  def handle_cast(
+        {:producer, %{duration: duration, producer_id: producer_id} = job},
+        %{jobs: jobs, consumers: []} = state
+      ) do
+    IO.puts("#{stats(state)} Queueing job #{duration} from #{producer_id}")
     {:noreply, %{state | jobs: [job | jobs]}}
+  end
+
+  defp stats(%{jobs: jobs, consumers: consumers}) do
+    inspect({length(jobs), length(consumers)})
   end
 end
